b5977b13f5b9c6bd836ecf5a1dc4611256a3bcf7
Aurelian2025 <aureliang@iceworksbathco.ca>
2025-12-08 14:13:02 -0500

    Create test-route.js

---
+++ pages/test-route.js
@@ -0,0 +1,9 @@
+// pages/test-route.js
+export default function TestRoute() {
+  return (
+    <main style={{ padding: 40, fontFamily: 'system-ui' }}>
+      <h1>Test route is alive</h1>
+      <p>If you see this, this deployment includes pages/test-route.js.</p>
+    </main>
+  );
+}
