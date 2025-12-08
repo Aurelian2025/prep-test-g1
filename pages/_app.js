c13a95719381aaf6ef8c112fd5be723892677b0b
Aurelian2025 <aureliang@iceworksbathco.ca>
2025-12-08 11:57:21 -0500

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,19 @@
+// pages/_app.js
+import { useState } from 'react';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+
+export default function MyApp({ Component, pageProps }) {
+  const [supabaseClient] = useState(() =>
+    createBrowserSupabaseClient()
+  );
+
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  );
+}
