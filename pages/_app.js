fd3409f99a80bb3907c265579a914a3cfddd1de3
Aurelian2025 <aureliang@iceworksbathco.ca>
2025-12-14 13:50:49 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -9,14 +9,21 @@ export default function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MFRVHSPH');</script>
-<!-- End Google Tag Manager —>
-
+      {/* Google Tag Manager */}
+      <Script
+        id="gtm"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-XXXX');
+          `,
+        }}
+      />
+      {/* End Google Tag Manager */}
 
       <SessionContextProvider
         supabaseClient={supabaseClient}
