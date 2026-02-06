764ec2dbce640e210d658b5ec646dc024860412a
Aurelian2025 <aureliang@iceworksbathco.ca>
2026-02-06 10:43:09 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -291,7 +291,7 @@ export default function PrepTestG1() {
   const supabase = useSupabaseClient();
 
   // Owner/master override (for you only)
-  const OWNER_PASSWORD = "Lucas";
+  const OWNER_PASSWORD = "Lucas1";
 
   // Access/login state
   const [hasAccess, setHasAccess] = useState(false);
